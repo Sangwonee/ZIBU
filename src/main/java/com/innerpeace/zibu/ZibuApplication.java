18efa4dba22d4f9aa40df313d8f2f81f684c8cbb
Sangwonee <sangw12477@gmail.com>
2025-07-09 15:37:31 +0900

    initialize projcet

---
+++ src/main/java/com/innerpeace/zibu/ZibuApplication.java
@@ -0,0 +1,13 @@
+package com.innerpeace.zibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZibuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZibuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innerpeace/zibu/ZibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innerpeace.zibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZibuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
