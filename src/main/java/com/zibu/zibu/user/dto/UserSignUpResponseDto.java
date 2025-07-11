2ac0f1b9ffca9dfb1d281fb53b46bcd9983d4566
Sangwonee <sangw12477@gmail.com>
2025-07-11 18:21:20 +0900

    style: use Lombok @AllArgsConstructor instead of manual constructor

--- src/main/java/com/zibu/zibu/user/dto/UserSignUpResponseDto.java
+++ src/main/java/com/zibu/zibu/user/dto/UserSignUpResponseDto.java
@@ -1,19 +1,15 @@
 package com.zibu.zibu.user.dto;
 
 import com.zibu.zibu.user.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserSignUpResponseDto {
     private Long id;
     private String email;
     private String nickname;
-
-    private UserSignUpResponseDto(Long id, String email, String nickname) {
-        this.id = id;
-        this.email = email;
-        this.nickname = nickname;
-    }
 
     public static UserSignUpResponseDto from(User user) {
         return new UserSignUpResponseDto(user.getId(), user.getEmail(), user.getNickname());
